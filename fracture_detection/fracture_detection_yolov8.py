64c7f79349c60e5109519040cfac7d68962ba86f
XAW2901120081 <151419300+XAW2901120081@users.noreply.github.com>
2024-10-31 14:27:46 +0800

    Update fracture_detection_yolov8.py

--- fracture_detection/fracture_detection_yolov8.py
+++ fracture_detection/fracture_detection_yolov8.py
@@ -90,7 +90,7 @@ def main():
             progress = st.progress(0)
             progress_text = st.empty()  # 用于显示百分比
 
-            model = YOLO("C:\\Users\\18301\\OneDrive\\Desktop\\FractureDetection1.2\\fracture_detection\\yolov8m_10.16.pt")  # Load model here
+            model = YOLO('fracture_detection/yolov8m_10.16.pt')  # Load model here
 
             # 使用线程池并行处理每张图片
             images_to_display = []
